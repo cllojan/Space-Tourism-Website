95ee364de0fa60421d54e1aef762c9b93fccda86
Lummy-rs <cesar.lojan8@gmail.com>
2022-05-26 20:01:38 -0500

    space page

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports ={
+    entry : ["babel-polyfill","./src/index.js"],
+    output:{
+        path: path.resolve(__dirname,"dist"),
+        filename:"build.js"
+    },
+    resolve:{
+        extensions:[".js",".jsx"]
+    },
+    module:{
+        rules:[
+            {
+                test:/\.(js|jsx)$/,
+                exclude:/node_modules/,
+                use:{
+                    loader:"babel-loader"
+                },
+            },
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader:"html-loader"
+                    }
+                ]
+            },
+            {
+                use:["style-loader","css-loader","sass-loader"],
+                test:/.(css|sass|scss)$/,
+            },
+        ],
+    },
+    devServer:{
+        historyApiFallback:true,
+    },
+    puglins:[
+        new HtmlWebpackPlugin({
+            template:"./plugin/index.html",
+            filename:"./index.html"
+        })
+    ]
+}
